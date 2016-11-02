cd92c4ed6007aae811ac46a051fa68a813596cd7
Jose Sebastian Battig <jsbattig@convey.com>
2016-11-02 14:25:19 -0500

    B-0 cleaned up

--- Src/GTestStyleOutput/GTestStyleJUnitSuite.java
+++ Src/GTestStyleOutput/GTestStyleJUnitSuite.java
@@ -1,5 +1,6 @@
 package GTestStyleOutput;
 
+import org.junit.runner.Description;
 import org.junit.runner.Runner;
 import org.junit.runner.notification.RunNotifier;
 import org.junit.runners.Suite;
@@ -7,25 +8,62 @@ import org.junit.runners.model.InitializationError;
 import org.junit.runners.model.RunnerBuilder;
 
 public class GTestStyleJUnitSuite extends Suite{
+    public static long SuiteStartTime = System.currentTimeMillis();
+
     public GTestStyleJUnitSuite(Class<?> klass, RunnerBuilder builder) throws InitializationError {
         super(klass, builder);
     }
 
+    private void PrintRunPreface() {
+        System.err.println("[==========] Running JUnit tests");
+        System.err.println("[----------] Classes loaded:");
+        for(Runner runner : getChildren()) {
+            System.err.println("             " + runner.getDescription().getClassName());
+        }
+    }
+
+    private static void PrintSuitePreface(Runner runner){
+        System.err.println("[----------] Running " + runner.testCount() + " tests from suite " + runner.getDescription().getClassName());
+        GTestStyleJUnitTestCase.ClassTestCount = 0;
+        GTestStyleJUnitTestCase.ClassStartTime = System.currentTimeMillis();
+    }
+
+    private static void PrintSuitePostface(Runner runner) {
+        System.err.println("[----------] " + GTestStyleJUnitTestCase.ClassTestCount + " test from " + runner.getDescription().getClassName() + " (" + (System.currentTimeMillis() - GTestStyleJUnitTestCase.ClassStartTime) + " ms total)");
+        System.err.println();
+    }
+
+    private static void PrintRunPostface() {
+        System.err.println("[----------] Global JUnit test environment tear-down");
+        System.err.println("[==========] " + GTestStyleJUnitTestCase.TotalTestCount + " test cases run. (" + (System.currentTimeMillis() - SuiteStartTime) + " ms total)");
+
+        if(GTestStyleJUnitTestCase.PassedCount > 0)
+            System.err.println("[  PASSED  ] " + GTestStyleJUnitTestCase.PassedCount + " tests.");
+        if(GTestStyleJUnitTestCase.SkippedCount > 0) {
+            System.err.println("[  SKIPPED ] " + GTestStyleJUnitTestCase.SkippedCount + " tests.");
+            for(Description description : GTestStyleJUnitTestCase.SkippedTests) {
+                System.err.println("[  SKIPPED ] " + description.getClassName());
+            }
+        }
+        if(GTestStyleJUnitTestCase.FailedCount > 0) {
+            System.err.println("[  FAILED  ] " + GTestStyleJUnitTestCase.FailedCount + " tests.");
+            for(Description description : GTestStyleJUnitTestCase.FailedTests) {
+                System.err.println("[  FAILED  ] " + description.getClassName());
+            }
+        }
+    }
+
     @Override
     protected void runChild(Runner runner, RunNotifier notifier) {
+        if(getChildren().get(0) == runner)
+            PrintRunPreface();
+
+        PrintSuitePreface(runner);
         runner.run(notifier);
-        if(getChildren().get(getChildren().size() - 1) == runner) {
-            System.err.println("[----------] " + GTestStyleJUnitTestCase.ClassTestCount + " test from " + GTestStyleJUnitTestCase.CurrentRunningClass + " (" + (System.currentTimeMillis() - GTestStyleJUnitTestCase.ClassStartTime) + " ms total)");
-            System.err.println("");
-            System.err.println("[----------] Global JUnit test environment tear-down");
-            System.err.println("[==========] " + GTestStyleJUnitTestCase.TotalTestCount + " test cases run. (" + (System.currentTimeMillis() - GTestStyleJUnitTestCase.SuiteStartTime) + " ms total)");
-            if(GTestStyleJUnitTestCase.SkippedCount > 0)
-                System.err.println("[  SKIPPED ] " + GTestStyleJUnitTestCase.SkippedCount + " tests.");
-            if(GTestStyleJUnitTestCase.PassCount > 0)
-                System.err.println("[  PASSED  ] " + GTestStyleJUnitTestCase.PassCount + " tests.");
-            if(GTestStyleJUnitTestCase.FailedCount > 0)
-                System.err.println("[  FAILED  ] " + GTestStyleJUnitTestCase.FailedCount + " tests.");
-        }
+        PrintSuitePostface(runner);
+
+        if(getChildren().get(getChildren().size() - 1) == runner)
+            PrintRunPostface();
     }
 }
 
--- Src/GTestStyleOutput/GTestStyleJUnitTestCase.java
+++ Src/GTestStyleOutput/GTestStyleJUnitTestCase.java
@@ -5,67 +5,53 @@ import org.junit.rules.TestName;
 import org.junit.rules.TestWatcher;
 import org.junit.runner.Description;
 
+import java.util.LinkedList;
+
 public class GTestStyleJUnitTestCase {
-    public static long ClassStartTime;
     private static long _startTime;
-    public static int ClassTestCount;
-    public static String CurrentRunningClass = "";
+
     public static int TotalTestCount = 0;
-    public static long SuiteStartTime = System.currentTimeMillis();
-    public static int PassCount = 0;
+    public static long ClassStartTime = 0;
+    public static int ClassTestCount = 0;
+    public static int PassedCount = 0;
     public static int FailedCount = 0;
     public static int SkippedCount = 0;
+    public final static LinkedList<Description> FailedTests = new LinkedList<>();
+    public final static LinkedList<Description> SkippedTests = new LinkedList<>();
 
     @Rule
     public TestWatcher testWatcher = new TestWatcher() {
-        private String TestName(Description description) {
-            String methodName = description.getMethodName();
-            String className = description.getClassName();
-            className = className.substring(className.lastIndexOf('.') + 1);
-            return className + "." + methodName;
-        }
-
         @Rule
         public TestName testName = new TestName();
 
         @Override
         protected void starting(Description description) {
-            if(!CurrentRunningClass.equals(description.getClassName())) {
-                if(CurrentRunningClass.equals(""))
-                    System.err.println("[==========] Running JUnit tests");
-                else {
-                    System.err.println("[----------] " + ClassTestCount + " test from " + CurrentRunningClass + " (" + (System.currentTimeMillis() - ClassStartTime) + " ms total)");
-                    System.err.println();
-                }
-                System.err.println("[----------] Running tests from class " + description.getClassName());
-                CurrentRunningClass = description.getClassName();
-                ClassTestCount = 0;
-                ClassStartTime = System.currentTimeMillis();
-            }
             _startTime = System.currentTimeMillis();
-            System.err.println("[ RUN      ] " + TestName(description));
+            System.err.println("[ RUN      ] " + description.getMethodName());
             ClassTestCount++;
             TotalTestCount++;
         }
 
         @Override
         protected void succeeded(Description description) {
-            System.err.println("[       OK ] " + TestName(description) + " (" + (System.currentTimeMillis() - _startTime) + " ms)");
-            PassCount++;
+            System.err.println("[       OK ] " + description.getMethodName() + " (" + (System.currentTimeMillis() - _startTime) + " ms)");
+            PassedCount++;
         }
 
         @Override
         protected void failed(Throwable e, Description description) {
             System.err.println(e.getMessage());
-            System.err.println("[  FAILED  ] " + TestName(description) + " (" + (System.currentTimeMillis() - _startTime) + " ms)");
+            System.err.println("[  FAILED  ] " + description.getMethodName() + " (" + (System.currentTimeMillis() - _startTime) + " ms)");
             FailedCount++;
+            FailedTests.add(description);
         }
 
         @Override
         protected void skipped(org.junit.AssumptionViolatedException e, Description description) {
             System.err.println(e.getMessage());
-            System.err.println("[  SKIPPED ] " + TestName(description));
+            System.err.println("[  SKIPPED ] " + description.getMethodName());
             SkippedCount++;
+            SkippedTests.add(description);
         }
     };
 }
